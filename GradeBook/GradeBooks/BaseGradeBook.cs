cc23e6c41e6176b2a11457b5d4838226e11c2ee7
Giancarlo Rhodes <giancarlo.rhodes@gmail.com>
2018-05-25 11:20:15 -0500

    adding Type of GradeBookType

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,6 +14,9 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
+
+        public GradeBookType Type { get; set; }
+
         public BaseGradeBook(string name)
         {
             Name = name;
